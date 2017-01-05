f2bdf1f4839889ef7b6fa7e4cf8f5be1b5a76c5f
Kevin Finley <kftwotwo@gmail.com>
2017-01-04 19:59:22 -0800

    able to get humidity

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -4,9 +4,8 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(response);
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
     });
   });
 });
