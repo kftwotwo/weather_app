ff6db87a885ab2c747d674ca18e1b8931aafa550
Kevin Finley <kftwotwo@gmail.com>
2017-01-04 21:50:11 -0800

    add imperial measurements

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,13 +1,13 @@
-var apiKey = process.env.OPEN_WEATHER_MAP_API_KEY;
+var apiKey = 'XXXXXXXXXXXXXXXXXXXX';
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&appid=' + apiKey, function(response) {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + ' &units=imperial' + '&appid=' + apiKey, function(response) {
       $('.showHumidity').text("The humidity in " + city + " is " + response.main.humidity + "%");
-      $('.showHigh').text("The High of  " + city + " is " + response.main.temp_max);
-      $('.showLow').text("The Low of  " + city + " is " + response.main.temp_min);
+      $('.showHigh').text("The High of  " + city + " is " + response.main.temp_max + " F");
+      $('.showLow').text("The Low of  " + city + " is " + response.main.temp_min+ " F");
       console.log(parseInt(response.id));
     });
   });
